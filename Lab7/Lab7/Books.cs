c5c5a76de9e007a7f864df1db98ec615a3739433
Nguyenphu1807 <Nguyenphuyh2812@users.noreply.github.com>
2020-09-15 14:10:20 +0700

    bt

---
+++ Lab7/Lab7/Books.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+   public class Books
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+
+
+    public class BookManager
+    {
+        public static List<Books> GetBooks()
+        {
+            var books = new List<Books>();
+
+            books.Add(new Books { BookId = 1, Title = "ABC", Author = "abc", CoverImage = "Assets/b2.jpg" });
+            books.Add(new Books { BookId = 2, Title = "ABCD", Author = "abcd", CoverImage = "Assets/b2.jpg" });
+            books.Add(new Books { BookId = 3, Title = "ABCDE", Author = "abcde", CoverImage = "Assets/b2.jpg" });
+        }
+    }
+}
---
+++ Lab8/Lab8/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Lab8.Model;
+using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Lab8
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Newitem> Newitems;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Newitems = new ObservableCollection<Newitem>();
+        }
+
+        private void HumburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Financial.IsSelected)
+            {
+                NewsManager.GetNews("Financial", Newitems);
+                TitleTextBlock.Text = "Financial";
+            }
+            else if (Food.IsSelected)
+            {
+                NewsManager.GetNews("Food", Newitems);
+                TitleTextBlock.Text = "Food";
+            }
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Financial.IsSelected = true;
+        }
+    }
+}
