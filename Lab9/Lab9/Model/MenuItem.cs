d9d51a42574db4265d6b48262164192e45b5cb2f
Nguyenphu1807 <Nguyenphuyh2812@users.noreply.github.com>
2020-09-17 22:52:43 +0700

    bt

---
+++ Lab9/Lab9/Model/MenuItem.cs
@@ -0,0 +1,15 @@
+﻿using Lab9.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9.Model
+{
+    public class MenuItem
+    {
+        public string IconFile { get; set; }
+        public SoundCategory Category { get; set; }
+    }
+}
